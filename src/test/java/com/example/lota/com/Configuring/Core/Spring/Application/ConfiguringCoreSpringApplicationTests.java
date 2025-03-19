a365f93d5065b5163f6dc762d7e63a517591e8d7
lota <christiansolomon592@gmail.com>
2025-03-19 22:13:14 +0100

    setting up project

---
+++ src/test/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lota.com.Configuring.Core.Spring.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguringCoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
