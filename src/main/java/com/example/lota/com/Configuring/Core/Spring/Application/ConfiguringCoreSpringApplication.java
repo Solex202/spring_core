40e8213b769d7c83ad8a79de74535e7630987587
lota <christiansolomon592@gmail.com>
2025-03-19 22:13:04 +0100

    setting up project

---
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.lota.com.Configuring.Core.Spring.Application;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfiguringCoreSpringApplication {
+
+	public static void main(String[] args) {
+
+		var context = new AnnotationConfigApplicationContext();
+	}
+
+}
