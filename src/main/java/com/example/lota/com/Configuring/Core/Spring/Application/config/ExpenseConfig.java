78063000e8e13e0e38200f5178ffc86309bdf785
lota <christiansolomon592@gmail.com>
2025-03-20 10:46:55 +0100

    adding beans to application context

---
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
@@ -0,0 +1,24 @@
+package com.example.lota.com.Configuring.Core.Spring.Application.config;
+
+import com.example.lota.com.Configuring.Core.Spring.Application.model.Expense;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ExpenseConfig {
+
+    @Bean
+    @Primary
+    public Expense expense() {
+        Expense exp = new Expense();
+        exp.setName("Bills");
+        return exp;
+    }
+    @Bean("foodie")
+    public Expense expense2() {
+        Expense exp = new Expense();
+        exp.setName("Food");
+        return exp;
+    }
+}
