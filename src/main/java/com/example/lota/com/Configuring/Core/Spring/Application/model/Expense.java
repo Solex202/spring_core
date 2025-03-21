f8acaabba05b82d9110c7e96cbe2aef9a0699129
lota <christiansolomon592@gmail.com>
2025-03-21 05:56:14 +0100

    adding database connection beans

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/model/Expense.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/model/Expense.java
@@ -1,26 +1,66 @@
 package com.example.lota.com.Configuring.Core.Spring.Application.model;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table(name = "expense")
+@Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
 
-    public String getName() {
-        return name;
+    private Category category;
+
+    private double amount;
+
+    public Expense() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Expense(Long id, String description, Category category, int amount) {
+        this.id = id;
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     @Override
     public String toString() {
         return "Expense{" +
-                "name='" + name + '\'' +
+                "name='" + description + '\'' +
                 '}';
     }
-
-    private String name;
-
-    private Category category;
 }
