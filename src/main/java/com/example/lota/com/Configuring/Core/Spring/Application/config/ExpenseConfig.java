65c7b79c3813476a12cf09a5cd28303dbbaedb5c
lota <christiansolomon592@gmail.com>
2025-03-21 05:44:13 +0100

    adding database connection beans

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @Component
@@ -28,5 +31,28 @@ public class ExpenseConfig {
         dataSource.setUsername("postgres");
         dataSource.setPassword("password");
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        sessionFactory.setPackagesToScan("com.example.lota.com.Configuring.Core.Spring.Application.model");
+        return sessionFactory;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        return properties;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
     }
 }
