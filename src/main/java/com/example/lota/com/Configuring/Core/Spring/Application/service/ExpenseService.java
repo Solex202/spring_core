d3dca88c90c5676359c96889cc94866582a7c50a
lota <christiansolomon592@gmail.com>
2025-03-21 15:38:38 +0100

    implementing business logic for expense tracker

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/service/ExpenseService.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/service/ExpenseService.java
@@ -2,17 +2,19 @@ package com.example.lota.com.Configuring.Core.Spring.Application.service;
 
 import com.example.lota.com.Configuring.Core.Spring.Application.model.Expense;
 
+import java.util.List;
+
 public interface ExpenseService {
 
     public Expense addExpense(Expense expense);
 
     public void updateExpense(Expense expense);
 
-    public void deleteExpense();
+    public void deleteExpense(long id);
 
     public void getExpense();
 
-    public void getAllExpenses();
+    public List<Expense> getAllExpenses();
 
     public void getExpenseByCategory();
 
