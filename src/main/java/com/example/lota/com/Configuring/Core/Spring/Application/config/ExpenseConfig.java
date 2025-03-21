5c2398e32cabc75b17eb35bc1736aac13c06b624
lota <christiansolomon592@gmail.com>
2025-03-21 04:41:03 +0100

    adding database connection dependency

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
@@ -4,8 +4,10 @@ import com.example.lota.com.Configuring.Core.Spring.Application.model.Expense;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
 public class ExpenseConfig {
 
     @Bean
@@ -16,7 +18,7 @@ public class ExpenseConfig {
         return exp;
     }
     @Bean("foodie")
-    public Expense expense2() {
+    public Expense expense2( Expense expense) {
         Expense exp = new Expense();
         exp.setName("Food");
         return exp;
