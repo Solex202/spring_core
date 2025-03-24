27af465b7400fe33f15b67651626b451cfe75ff9
lota <christiansolomon592@gmail.com>
2025-03-24 16:27:04 +0100

    configuring application server

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/ExpenseConfig.java
@@ -7,23 +7,16 @@ import org.springframework.stereotype.Component;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.util.Properties;
 
-@EnableWebMvc
+//@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.example.lota.com.Configuring.Core.Spring.Application")
 public class ExpenseConfig {
-
-//    @Bean
-//    @Primary
-//    public Expense expense() {
-//        Expense exp = new Expense();
-//        exp.setDescription("Bills");
-//        return exp;
-//    }
 
     @Bean
    public DataSource dataSource() {
@@ -32,7 +25,8 @@ public class ExpenseConfig {
         dataSource.setUrl("jdbc:postgresql://127.0.0.1:5431/expensedb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
-
+        dataSource.setDefaultAutoCommit(false);
+        dataSource.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
         dataSource.setInitialSize(5);
         dataSource.setMaxTotal(10);
         dataSource.setMaxIdle(5);
@@ -51,7 +45,7 @@ public class ExpenseConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
         return properties;
     }
