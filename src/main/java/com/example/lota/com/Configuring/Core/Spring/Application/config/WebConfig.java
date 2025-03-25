f48b4d36d96901b1c2b7ae8cafb85bbecf4b9ddf
lota <christiansolomon592@gmail.com>
2025-03-25 15:09:30 +0100

    configuring application server - tomcat

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/WebConfig.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.example.lota.com.Configuring.Core.Spring.Application.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.lota.com.Configuring.Core.Spring.Application.controller")
 public class WebConfig {
+
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.setPort(8088);
+//        return tomcat;
+//    }
 }
