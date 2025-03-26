3539eda0e1082ca28eab763d29ac673f729aae55
lota <christiansolomon592@gmail.com>
2025-03-26 19:53:25 +0100

    configuring application server - tomcat

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
@@ -1,18 +1,27 @@
 package com.example.lota.com.Configuring.Core.Spring.Application;
 
-
 import com.example.lota.com.Configuring.Core.Spring.Application.config.ExpenseConfig;
-import com.example.lota.com.Configuring.Core.Spring.Application.model.Expense;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ConfiguringCoreSpringApplication {
+	public static void main(String[] args) throws Exception {
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8088);
+		Context ctx = tomcat.addContext("", System.getProperty("java.io.tmpdir"));
 
-	public static void main(String[] args) throws InterruptedException {
-		ApplicationContext context = new AnnotationConfigApplicationContext(ExpenseConfig.class);
-//		System.out.println(context.getBeansOfType(Expense.class));
-		Thread.currentThread().join();
+		// Create and configure Spring Web Application Context
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(ExpenseConfig.class); // Replace with your configuration class
+//		appContext.refresh();
+		// Add DispatcherServlet
+		DispatcherServlet servlet = new DispatcherServlet(appContext);
+		Tomcat.addServlet(ctx, "dispatcher", servlet);
+		ctx.addServletMappingDecoded("/*", "dispatcher");
 
+		tomcat.start();
+		tomcat.getServer().await();
 	}
-
 }
