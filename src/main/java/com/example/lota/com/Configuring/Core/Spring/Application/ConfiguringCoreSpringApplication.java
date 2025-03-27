884181461c4548ccd80b8732d932747be8308a21
lota <christiansolomon592@gmail.com>
2025-03-27 11:10:02 +0100

    configuring application server - tomcat

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
@@ -1,8 +1,9 @@
 package com.example.lota.com.Configuring.Core.Spring.Application;
 
 import com.example.lota.com.Configuring.Core.Spring.Application.config.ExpenseConfig;
-import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -10,20 +11,25 @@ public class ConfiguringCoreSpringApplication {
 	public static void main(String[] args) throws Exception {
 		Tomcat tomcat = new Tomcat();
 		tomcat.setPort(8080);
-		Context ctx = tomcat.addContext("", System.getProperty("java.io.tmpdir"));
 
-		// Create context WITHOUT manual refresh
-		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-		appContext.register(ExpenseConfig.class);
+		// Create proper web application context
+		Context ctx = tomcat.addWebapp("", System.getProperty("java.io.tmpdir"));
 
-		// Create servlet with the context
-		DispatcherServlet servlet = new DispatcherServlet(appContext);
+		// Initialize Spring context PROPERLY
+		WebApplicationContext appContext = createWebApplicationContext();
 
-		// Register servlet with Tomcat
-		Tomcat.addServlet(ctx, "dispatcher", servlet);
+		// Add Spring dispatcher servlet
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
+		Tomcat.addServlet(ctx, "dispatcher", dispatcherServlet);
 		ctx.addServletMappingDecoded("/*", "dispatcher");
 
 		tomcat.start();
 		tomcat.getServer().await();
+	}
+
+	private static WebApplicationContext createWebApplicationContext() {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(ExpenseConfig.class);
+		return context;
 	}
 }
