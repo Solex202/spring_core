38ea514af7f3d0615720336f396c8d2bc738c0c6
lota <christiansolomon592@gmail.com>
2025-03-28 11:11:31 +0100

    configuring application server - tomcat

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
@@ -3,14 +3,22 @@ package com.example.lota.com.Configuring.Core.Spring.Application;
 import com.example.lota.com.Configuring.Core.Spring.Application.config.ExpenseConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.Context;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.util.Arrays;
+
 public class ConfiguringCoreSpringApplication {
 	public static void main(String[] args) throws Exception {
+//		ApplicationContext context = new AnnotationConfigApplicationContext(ExpenseConfig.class);
+//		System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+
 		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8080);
+//		tomcat.setPort(8081);
+		tomcat.getServer().setPort(8081);
 
 		// Create proper web application context
 		Context ctx = tomcat.addWebapp("", System.getProperty("java.io.tmpdir"));
@@ -21,10 +29,18 @@ public class ConfiguringCoreSpringApplication {
 		// Add Spring dispatcher servlet
 		DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
 		Tomcat.addServlet(ctx, "dispatcher", dispatcherServlet);
-		ctx.addServletMappingDecoded("/*", "dispatcher");
+		ctx.addServletMappingDecoded("/", "dispatcher");
 
+		System.out.println("Tomcat starting...");
 		tomcat.start();
+		tomcat.getServer().getPort();
+		System.out.println("Tomcat host =======================>> " + tomcat.getHost());
+		System.out.println("Tomcat port =======================>> " + tomcat.getServer().getPort());
+		System.out.println("Tomcat started!");
+
 		tomcat.getServer().await();
+
+
 	}
 
 	private static WebApplicationContext createWebApplicationContext() {
