3aed9782af18a0138f8dffccd84bdf45d0887e33
lota <christiansolomon592@gmail.com>
2025-03-29 22:12:24 +0100

    configuring application server - tomcat

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/ConfiguringCoreSpringApplication.java
@@ -3,13 +3,9 @@ package com.example.lota.com.Configuring.Core.Spring.Application;
 import com.example.lota.com.Configuring.Core.Spring.Application.config.ExpenseConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.Context;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.util.Arrays;
 
 public class ConfiguringCoreSpringApplication {
 	public static void main(String[] args) throws Exception {
@@ -17,8 +13,8 @@ public class ConfiguringCoreSpringApplication {
 //		System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
 		Tomcat tomcat = new Tomcat();
-//		tomcat.setPort(8081);
-		tomcat.getServer().setPort(8081);
+		tomcat.setPort(8081);
+//		tomcat.getServer().setPort(8081);
 
 		Context ctx = tomcat.addWebapp("", System.getProperty("java.io.tmpdir"));
 
@@ -32,12 +28,10 @@ public class ConfiguringCoreSpringApplication {
 		tomcat.start();
 		tomcat.getServer().getPort();
 		System.out.println("Tomcat host =======================>> " + tomcat.getHost());
-		System.out.println("Tomcat port =======================>> " + tomcat.getServer().getPort());
+		System.out.println("Tomcat port =======================>> " + tomcat.getConnector().getPort());
 		System.out.println("Tomcat started!");
 
 		tomcat.getServer().await();
-
-
 	}
 
 	private static WebApplicationContext createWebApplicationContext() {
