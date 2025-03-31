f6a89aad12840ea43e74540aaf651702aee760ce
lota <christiansolomon592@gmail.com>
2025-03-31 21:26:46 +0100

    refactoring: Using dto to map to expense object

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/dto/CreateExpenseDto.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/dto/CreateExpenseDto.java
@@ -6,11 +6,19 @@ public class CreateExpenseDto {
 
     private String description;
 
-    private Category category;
+    private String category;
 
     private double amount;
 
     public void setDescription(String description){
         this.description = description;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public void setAmount(double amount){
+        this.amount = amount;
     }
 }
