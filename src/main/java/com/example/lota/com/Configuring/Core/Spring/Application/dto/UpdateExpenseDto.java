4805859d1893632242be98a1de341c84ebf3aa56
lota <christiansolomon592@gmail.com>
2025-04-03 12:08:40 +0100

    Implementing update expense endpoint

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/dto/UpdateExpenseDto.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/dto/UpdateExpenseDto.java
@@ -1,4 +1,41 @@
 package com.example.lota.com.Configuring.Core.Spring.Application.dto;
 
 public class UpdateExpenseDto {
+
+    private String description;
+
+    private String category;
+
+    private double amount;
+
+    public UpdateExpenseDto(String description, String category, double amount) {
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
 }
