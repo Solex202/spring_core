ff88601825592eea27c6ed0d80cb81efe8a9a950
lota <christiansolomon592@gmail.com>
2025-04-03 13:53:24 +0100

    Implementing update expense endpoint

--- src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/mappers/UserMapper.java
+++ src/main/java/com/example/lota/com/Configuring/Core/Spring/Application/mappers/UserMapper.java
@@ -3,11 +3,13 @@ package com.example.lota.com.Configuring.Core.Spring.Application.mappers;
 import com.example.lota.com.Configuring.Core.Spring.Application.dto.UpdateExpenseDto;
 import com.example.lota.com.Configuring.Core.Spring.Application.model.Expense;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
 
-    Expense updateExpense(UpdateExpenseDto updateExpenseDto, Expense expense);
+    Expense updateExpense(UpdateExpenseDto updateExpenseDto,@MappingTarget Expense expense);
 }
